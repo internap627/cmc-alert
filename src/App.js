0abb1877d7b28d3a20fc33376c2290e5160afe1b
github username <internap627@gmail.com>
2021-05-18 14:18:40 -0400

    edit email list

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 
 function App() {
   const [list, setList] = useState([]);
-  const [last, setLast] = useState({ id: 9903 });
+  const [last, setLast] = useState({ id: 9911 });
 
   const x = "7a930438-3d27-4ad8-98b1-c66578fab4a7";
 
@@ -32,7 +32,7 @@ function App() {
               coinList: coinList,
               emailList: "saidcryptog@gmail.com, cryptomood1@gmail.com",
               ccList:
-                "internap627@gmail.com, Thebrightbrains1@gmail.com, Bob123bill1231@gmail.com, cryptoguru11@hotmail.com, Abdulhakeem15th@gmail.com, Ishy_ozzy307@hotmail.com, Moaweys1010@gmail.com, Abubakara1996@gmail.com, Jeilani90@gmail.com, zcrypto03@gmail.com, adamelhaddad3@gmail.com, Cheekymarketuk@gmail.com",
+                "internap627@gmail.com, Thebrightbrains1@gmail.com, Bob123bill1231@gmail.com, cryptoguru11@hotmail.com, Abdulhakeem15th@gmail.com, Ishy_ozzy307@hotmail.com, Moaweys1010@gmail.com, Abubakara1996@gmail.com, Jeilani90@gmail.com, zcrypto03@gmail.com, adamelhaddad3@gmail.com, Cheekymarketuk@gmail.com, hillflos00@gmail.com",
             };
 
             setTimeout(function () {
